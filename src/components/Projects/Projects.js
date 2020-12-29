d41386417e799f25768d18762c780b02b4c1779f
YaybeNo <evanmatt.wong@gmail.com>
2020-12-28 19:00:39 -0500

    minorfixes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,33 +9,33 @@ function Projects() {
     <>
       <div className="background">
         <div data-aos="fade-up" data-aos-duration="600">
-          <div className="project-bar-container">
-            <div className="project-bar">
-              <h2>Portfolio</h2>
-            </div>
-          </div>
-        </div>
-        <div className="project">
-          <div className="project-grid">
-            <div data-aos="fade-up" data-aos-duration="600">
-              <div className="grid-item">
-                <img src={waterlooimg} alt="" />
-                <h2>Developing features for Open Source Libraries</h2>
-                <em>University of Waterloo</em>
+          <div className="project">
+            <div className="project-bar-container">
+              <div className="project-bar">
+                <h2>Portfolio</h2>
               </div>
             </div>
-            <div data-aos="fade-up" data-aos-delay="150">
-              <div className="grid-item">
-                <a
-                  href="https://github.com/eevanwong/League-Bot"
-                  target="_blank"
-                >
-                  <img src={leagueimg} alt="" />
-                </a>
-                <h2>League Stats Discord Bot</h2>
-                <h2>
-                  <em>Side Project</em>
-                </h2>
+            <div className="project-grid">
+              <div data-aos="fade-up" data-aos-duration="600">
+                <div className="grid-item">
+                  <img src={waterlooimg} alt="" />
+                  <h2>Developing features for Open Source Libraries</h2>
+                  <em>University of Waterloo</em>
+                </div>
+              </div>
+              <div data-aos="fade-up" data-aos-delay="150">
+                <div className="grid-item">
+                  <a
+                    href="https://github.com/eevanwong/League-Bot"
+                    target="_blank"
+                  >
+                    <img src={leagueimg} alt="" />
+                  </a>
+                  <h2>League Stats Discord Bot</h2>
+                  <h2>
+                    <em>Side Project</em>
+                  </h2>
+                </div>
               </div>
             </div>
           </div>
